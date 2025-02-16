9fc55d8a684d0948cbe29e5942907d262802af0c
SouleCode <souleymanetraore.22026@gmail.com>
2025-02-16 23:00:11 +0100

    Message de commit

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -18,7 +18,7 @@ import WorkSliderBtns from '@/components/WorkSliderBtns';
 const projects=[
   {
     num: "01",
-    category:"Front",
+    category:"IA & Web",
     title:"Plateforme de Génération de Recettes Intelligentes par IA",
     description:"Ce projet consiste en la conception et le développement d'une plateforme web innovante qui intègre un modèle d'intelligence artificielle (IA) pour générer des recettes personnalisées en fonction des ingrédients disponibles dans le réfrigérateur de l'utilisateur. L'objectif est de simplifier la préparation des repas tout en réduisant le gaspillage alimentaire.",
     stack:[
@@ -32,7 +32,7 @@ const projects=[
   },
   {
     num: "02",
-    category:"Front",
+    category:"IA & Mobile",
     title:"Application Mobile de Prédiction Agricole et Diagnostic des Plantes par IA",
     description:"Ce projet vise à développer une application mobile innovante qui aide les agriculteurs à optimiser leurs rendements agricoles et à détecter précocement les maladies des plantes grâce à l'intelligence artificielle (IA). L'objectif est de fournir un outil accessible et efficace pour améliorer la productivité agricole tout en réduisant les pertes liées aux maladies.",
     stack:[
@@ -50,13 +50,14 @@ const projects=[
   },
   {
     num: "03",
-    category:"Front",
-    title:"Project 3",
-    description:"La description du project en question",
+    category:"Full Stack web",
+    title:"Dévelopment d'un blog pour la boite Pro Htech",
+    description:"Le projet consiste à concevoir et développer un blog moderne et interactif pour PRO Htech, une entreprise spécialisée dans les solutions technologiques innovantes. Ce blog servira de plateforme pour partager des articles, des tutoriels, des actualités et des études de cas liés aux technologies, aux tendances du secteur et aux services proposés par PRO Htech. L'objectif est de renforcer la visibilité de l'entreprise, d'établir son autorité dans le domaine et d'engager une communauté active. Le blog sera responsive, optimisé pour le référencement (SEO) et intégrera des fonctionnalités telles que des commentaires, un système de recherche avancée et des partages sur les réseaux sociaux. Une interface d'administration intuitive permettra à l'équipe de PRO Htech de publier et de gérer facilement du contenu.",
     stack:[
-      {name:"HTML5"},
-      {name:"CSS3"},
-      {name:"JavaScript"}
+      {name:"HTML5&CSS"},
+      {name:"MySQL"},
+      {name:"JavaScript"},
+      {name:"PHP"},
     ],
     image:"/assets/images/proj3.png",
     live:"",
@@ -64,13 +65,15 @@ const projects=[
   },
   {
     num: "04",
-    category:"Front",
-    title:"Project 4",
-    description:"La description du project en question",
+    category:"Full Stack web",
+    title:"Development d'un porfilio",
+    description:"Ce projet vise à créer un portfolio professionnel et esthétique pour mettre en valeur les compétences, les réalisations et les projets d'un individu ou d'une entreprise. Le portfolio sera conçu pour offrir une expérience utilisateur fluide et intuitive, mettant en avant les travaux réalisés à travers des sections dédiées (projets, compétences, à propos, contact). Il sera entièrement responsive, s'adaptant à tous les appareils (desktop, tablette, mobile), et intègrera des animations modernes pour un rendu visuel attractif. L'objectif est de fournir une vitrine digitale qui reflète la créativité, l'expertise et la personnalité du propriétaire, tout en facilitant la prise de contact pour des opportunités professionnelles. Des fonctionnalités supplémentaires, comme un blog intégré ou un système de filtrage des projets, pourront être ajoutées selon les besoins",
     stack:[
-      {name:"HTML5"},
-      {name:"CSS3"},
-      {name:"JavaScript"}
+      {name:"Next.js"},
+      {name:"Tailwind CSS"},
+      {name:"Framer Motion"},
+      {name:"Vercel"}
+
     ],
     image:"/assets/images/proj4.png",
     live:"",
@@ -105,7 +108,7 @@ const Work=()=>{
                         </div>
                         {/* Project category */}
                         <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize'>
-                          {Projects.category}-Project
+                          {Projects.category}
                         </h2>
                         {/* project description */}
                         <p className='mt-4 text-lg text-white/60'>
